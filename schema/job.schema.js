dc92c0cc93affdca9b013174e3b49fe33f2394d3
RudranshSolanki <rudrasingh7318115@gmail.com>
2025-05-04 23:38:24 +0530

    updated schema

--- schema/job.schema.js
+++ schema/job.schema.js
@@ -4,6 +4,6 @@ import mongoose from "mongoose";
 export const jobSchema = mongoose.Schema({
     company:{type:String},
     result:{type:String,default:'applied',enum:['applied','Interview pending','rejected','Assignment Pending','Result Pending']},
-    data:{type:Date, default:Date.now()},
+    date:{type:Date, default:Date.now()},
  
 })
